697e98b80640dfe79079973cfd9cbf4e4ec596b0
nicksaravia <nick.saravia@willowtreeapps.com>
2022-12-23 11:56:26 -0500

    add dist in pre-push

--- dist/index.js
+++ dist/index.js
@@ -6,11 +6,21 @@ function $parcel$export(e, n, v, s) {
 }
 
 $parcel$export(module.exports, "Button", () => $6ab71aa90505fd93$export$353f5b6fc5456de1);
+$parcel$export(module.exports, "Link", () => $a2856290f36da6b9$export$a6c7ac8248d6e38a);
 
 
 const $6ab71aa90505fd93$export$353f5b6fc5456de1 = ({ children: children , ...rest })=>{
     return /*#__PURE__*/ (0, $8zHUo$reactjsxruntime.jsx)("button", {
         ...rest,
+        children: children
+    });
+};
+
+
+
+const $a2856290f36da6b9$export$a6c7ac8248d6e38a = ({ children: children , ...props })=>{
+    return /*#__PURE__*/ (0, $8zHUo$reactjsxruntime.jsx)("a", {
+        ...props,
         children: children
     });
 };
--- dist/module.js
+++ dist/module.js
@@ -12,6 +12,15 @@ const $cbe0cea8f921a013$export$353f5b6fc5456de1 = ({ children: children , ...res
 
 
 
+const $cdaa343c5cfc28fa$export$a6c7ac8248d6e38a = ({ children: children , ...props })=>{
+    return /*#__PURE__*/ (0, $hgUW1$jsx)("a", {
+        ...props,
+        children: children
+    });
+};
 
-export {$cbe0cea8f921a013$export$353f5b6fc5456de1 as Button};
+
+
+
+export {$cbe0cea8f921a013$export$353f5b6fc5456de1 as Button, $cdaa343c5cfc28fa$export$a6c7ac8248d6e38a as Link};
 //# sourceMappingURL=module.js.map
