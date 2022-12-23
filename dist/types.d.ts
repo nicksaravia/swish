d6a130a1eba402f8aee7c9f5628fbc61ac05b468
nicksaravia <nick.saravia@willowtreeapps.com>
2022-12-23 13:49:35 -0500

    delete dist before parcel build

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -1,7 +1,11 @@
-import React, { ButtonHTMLAttributes } from "react";
+import React, { ButtonHTMLAttributes, AnchorHTMLAttributes } from "react";
 interface ButtonProps {
     children: React.ReactNode;
 }
 export const Button: React.FC<ButtonProps & ButtonHTMLAttributes<HTMLButtonElement>>;
+interface LinkProps {
+    children: React.ReactNode;
+}
+export const Link: React.FC<LinkProps & AnchorHTMLAttributes<HTMLAnchorElement>>;
 
 //# sourceMappingURL=types.d.ts.map
